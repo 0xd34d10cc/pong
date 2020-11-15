1825c2c05ebb6b872e9615efb36d7e121e78f189
0xd34d10cc <0xd34d10cc@gmail.com>
2020-11-15 02:23:05 +0000

    Explicitly specify C standard in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-clang -o pong src/*.c -Werror=implicit-function-declaration -Werror=implicit-int -lSDL2
+clang -o pong src/*.c -std=c11 -Werror=implicit-function-declaration -Werror=implicit-int -lSDL2
