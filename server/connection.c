4e77fa734ef22046c714519a305bfac6f4d6bf80
0xd34d10cc <0xd34d10cc@gmail.com>
2020-11-15 02:40:09 +0000

    Fix warnings in server. Remove server binary from VCS.

--- server/connection.c
+++ server/connection.c
@@ -16,77 +16,77 @@
 
 void run(char* ip, char* port, struct ConnectionMap* con_map) {
   static int session_counter = 0;
-  struct sockaddr_in addr = {0};    
-  addr.sin_family = AF_INET;    
-  addr.sin_port = htons(atoi(port));    
-  addr.sin_addr.s_addr = inet_addr(ip);    
-  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);    
-  
-  if (sock == -1) {    
-    perror("sock is not ready to be used");    
-    close(sock);    
-    return;    
-  }    
-  
-  printf("start binding\n");    
-    
-  if (bind(sock, (struct sockaddr*) &addr, sizeof addr) == -1) {    
-    perror("can't bind to the address");    
-    close(sock);    
-    return;    
-  }    
-    
-  printf("start listening\n");    
-  if (listen(sock, 20) == -1) {    
-    perror("error is occured while initialization of listen()");    
-    return;    
-  }    
-    
-  socklen_t socklen = sizeof addr;    
-  struct sockaddr_in player1_addr = {0};    
-  struct sockaddr_in player2_addr = {0};    
-  printf("starting accepting\n");    
-  int player1_sock = accept(sock, &player1_addr, &socklen);    
-  printf("Accept player1\n");    
-  int player2_sock = accept(sock, &player2_addr, &socklen);    
-  printf("Accept player2\n");    
-    
-  if (player1_sock == -1 || player2_sock == -1) {    
-    perror("accept error");    
-    close(sock);    
-    return;    
-  }    
-      
-  struct ConnectionStorage con_storage = {0};        
-      
-  char* player1_str = inet_ntoa(player1_addr.sin_addr);    
-  memcpy(con_storage.player1_addr, player1_str, strlen(player1_str) + 1);    
-      
-  char* player2_str = inet_ntoa(player2_addr.sin_addr);    
-  memcpy(con_storage.player2_addr, player2_str, strlen(player2_str) + 1);    
-    
-  char buf[BUFFSIZE];    
-  ssize_t readden = recv(player1_sock, buf, BUFFSIZE, 0);    
-    
-  if (readden == 0) {    
-    printf("session was killed by user (P1)\n");    
-    return;    
+  struct sockaddr_in addr = {0};
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(atoi(port));
+  addr.sin_addr.s_addr = inet_addr(ip);
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+  if (sock == -1) {
+    perror("sock is not ready to be used");
+    close(sock);
+    return;
+  }
+
+  printf("start binding\n");
+
+  if (bind(sock, (struct sockaddr*) &addr, sizeof addr) == -1) {
+    perror("can't bind to the address");
+    close(sock);
+    return;
+  }
+
+  printf("start listening\n");
+  if (listen(sock, 20) == -1) {
+    perror("error is occured while initialization of listen()");
+    return;
+  }
+
+  socklen_t socklen = sizeof addr;
+  struct sockaddr_in player1_addr = {0};
+  struct sockaddr_in player2_addr = {0};
+  printf("starting accepting\n");
+  int player1_sock = accept(sock, (struct sockaddr*)&player1_addr, &socklen);
+  printf("Accept player1\n");
+  int player2_sock = accept(sock, (struct sockaddr*)&player2_addr, &socklen);
+  printf("Accept player2\n");
+
+  if (player1_sock == -1 || player2_sock == -1) {
+    perror("accept error");
+    close(sock);
+    return;
+  }
+
+  struct ConnectionStorage con_storage = {0};
+
+  char* player1_str = inet_ntoa(player1_addr.sin_addr);
+  memcpy(con_storage.player1_addr, player1_str, strlen(player1_str) + 1);
+
+  char* player2_str = inet_ntoa(player2_addr.sin_addr);
+  memcpy(con_storage.player2_addr, player2_str, strlen(player2_str) + 1);
+
+  char buf[BUFFSIZE];
+  ssize_t readden = recv(player1_sock, buf, BUFFSIZE, 0);
+
+  if (readden == 0) {
+    printf("session was killed by user (P1)\n");
+    return;
   } else if (readden < 0) {
     perror("internal recv error (?)");
     return;
   } else {
-    
+
     // parse messages
     // TODO: recv_all();
-    
+
     int msg_size = 0;
     memcpy(&msg_size, buf, sizeof(int));
-    
+
     char* msg = malloc(msg_size);
     memcpy(msg, buf, msg_size);
 
     enum MessageType msg_type = getMessageType(msg);
-    
+
     if (msg_type == CreateGameSession) {
       struct CreateGameSessionMsg cgs_msg = {0};
       memcpy(&cgs_msg.pw_size,  msg + 4, sizeof(int));
@@ -101,7 +101,7 @@ void run(char* ip, char* port, struct ConnectionMap* con_map) {
       memcpy(con_storage.pw, cgs_msg.pw, cgs_msg.pw_size);
       int session_id = session_counter++;
 
-      insert(con_map, session_id, &con_storage);  
+      insert(con_map, session_id, &con_storage);
       //send_session(session_id);
     }
   }
--- server/main.c
+++ server/main.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
 
 #include "messages.h"
 #include "connection_utils.h"
@@ -23,20 +16,19 @@ enum StatusCode {
 };
 
 void handleCreateGameSession(struct CreateGameSessionMsg* msg, struct ConnectionMap* con_map) {
-  
+
 }
 
 int main(int argc, char* argv[]) {
   static int session_counter = 0;
   // ./server 127.0.0.1 1337
-  printf("starting with %s : %s\n", argv[1], argv[2]);
   if (argc != 3) {
     printf("invalid ip or port\n");
     return -1;
   }
-  
-  struct ConnectionMap con_map = {0};
 
+  printf("starting with %s : %s\n", argv[1], argv[2]);
+  struct ConnectionMap con_map = {0};
   run(argv[1], argv[2],  &con_map);
 
   return 0;
