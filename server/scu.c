cf853716c9da8574349a26771e5c09d58bbc3f2f
Pylev Nikita <pl0q1n@gmail.com>
2020-12-22 01:41:37 +0300

    Advance progress in active games periodically (#21)

--- server/scu.c
+++ server/scu.c
@@ -1,6 +1,7 @@
 #include "utils/log.c"
 #include "utils/panic.c"
 #include "game/protocol.c"
+#include "game/vec2.c"
 #include "game/game.c"
 #include "net/tcp_stream.c"
 #include "net/tcp_listener.c"
